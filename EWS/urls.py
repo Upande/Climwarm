9c09b8e0310045420d668737bc361722626a4451
pmatogo <pmatogo@gmail.com>
2015-03-05 10:30:48 +0300

    Enhancement:  Adding separate drought and flood hazard pages adding drought_hazard template adding flood_hazard template createing new urls for drought and flood hazard plus adding respective view classes

--- EWS/urls.py
+++ EWS/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from .views import DefaultView, HomeView
+from .views import *
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', DefaultView.as_view(), name='default'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^home', HomeView.as_view(), name='home'),
+    url(r'^drought', DroughtView.as_view(), name='drought_home'),
+    url(r'^flood', FloodView.as_view(), name='flood_home'),
 	url(r'^accounts/login/$', 'django.contrib.auth.views.login', 
         {'template_name': 'login.html'}, name='auth_login'),
 	url(r'^accounts/logout/$', 'django.contrib.auth.views.logout_then_login', name='auth_logout'),
