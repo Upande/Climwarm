ec9b62a2e2881198669dd57a6c69cfcf42b2e5ef
pmatogo <pmatogo@gmail.com>
2015-03-06 16:15:36 +0300

    Feature:  Sendig SMS from shape file adding Africastalking API class post method in views to handle post action to send sms/email

--- EWS/views.py
+++ EWS/views.py
@@ -1,5 +1,8 @@
-from django.views.generic import TemplateView, FormView
+from django.views.generic import View, TemplateView
 from django.shortcuts import redirect
+from django.core.urlresolvers import reverse
+
+from AfricasTalkingGateway import *
 
 class DefaultView(TemplateView):
 	template_name = 'default.html'
@@ -8,7 +11,25 @@ class DroughtView(TemplateView):
 	template_name = 'drought_hazard.html'
 
 	def post(self, request, *args, **kwargs):
-		return redirect('drought_home')
+		try:
+			import urllib2
+			import simplejson
+			req = urllib2.Request('http://maps.virtualkenya.org/geoserver/wfs?srsName=EPSG%3A4326&typename=geonode%3Adrought_constituencies&outputFormat=json&version=1.0.0&service=WFS&request=GetFeature')
+			opener = urllib2.build_opener()
+			f = opener.open(req)
+			data = simplejson.load(f)
+			gateway = AfricasTalkingGateway('upande', '5da433821fa3efe8d793bfb95da80e63a4f2909c559ea58cc5a8e096fa568965')
+			for item in data['features']:
+				msisdn = item['properties']['Snippet']
+				#print item['properties']['Snippet']
+				print msisdn
+				gateway.sendMessage(msisdn, 'Drought alert SMS from Early Warning System')
+			gateway.sendMessage('+254722659526', 'Drought alert SMS from Early Warning System')
+		except Exception as ex:
+			print 'exception: {0}'.format(ex.message)
+			pass
+
+		return redirect(reverse('drought_home'))
 
 class FloodView(TemplateView):
 	template_name = 'flood_hazard.html'
