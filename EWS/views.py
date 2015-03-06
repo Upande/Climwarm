a34ee5befd0a33f8005190343f55bc27c7ad7e13
Peter Matogo <pmatogo@gmail.com>
2015-03-06 10:30:52 +0000

    adding alert button

--- EWS/views.py
+++ EWS/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
+from django.shortcuts import redirect
 
 class DefaultView(TemplateView):
 	template_name = 'default.html'
@@ -6,5 +7,11 @@ class DefaultView(TemplateView):
 class DroughtView(TemplateView):
 	template_name = 'drought_hazard.html'
 
+	def post(self, request, *args, **kwargs):
+		return redirect('drought_home')
+
 class FloodView(TemplateView):
 	template_name = 'flood_hazard.html'
+
+	def post(self, request, *args, **kwargs):
+		return redirect('drought_home')
