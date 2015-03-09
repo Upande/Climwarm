3361261d8cd87584720efba7b64e67eceac0b64a
Peter Matogo <pmatogo@gmail.com>
2015-03-09 16:33:14 +0000

    enabling alerts on both drought and flood hazard pages

--- EWS/views.py
+++ EWS/views.py
@@ -17,7 +17,9 @@ class DroughtView(TemplateView):
 			import simplejson
 			postdata = request.POST.copy()
 			alert_type = postdata['alert_type']
+			hazard_type = postdata['hazard_type']
 			print 'ALERT TYPE: {0}'.format(alert_type)
+			print 'HAZARD TYPE: {0}'.format(hazard_type)
 			req = urllib2.Request('http://maps.virtualkenya.org/geoserver/wfs?srsName=EPSG%3A4326&typename=geonode%3Adrought_constituencies&outputFormat=json&version=1.0.0&service=WFS&request=GetFeature')
 			opener = urllib2.build_opener()
 			f = opener.open(req)
@@ -27,13 +29,13 @@ class DroughtView(TemplateView):
 				for item in data['features']:
 					msisdn = item['properties']['SNIPPET']
 					print msisdn
-					gateway.sendMessage(msisdn, 'Drought alert SMS from Early Warning System')
-				gateway.sendMessage('+254722659526', 'Drought alert SMS from Early Warning System')
+					gateway.sendMessage(msisdn, '{0} alert SMS from Early Warning System'.format(hazard_type))
+				gateway.sendMessage('+254722659526', '{0} alert SMS from Early Warning System'.format(hazard_type))
 			elif (alert_type == 'EMAIL'):
 				mandrill_client = mandrill.Mandrill('17dld1KJhCspkCV6zlfbtA')
 				message = {
 					'from_email': 'ews@upande.com',
-					'subject':'Drought early warning',
+					'subject':'{0} early warning'.format(hazard_type),
 					'text':'This is an early warning email'
 				}
 				to_list = []
@@ -55,4 +57,43 @@ class FloodView(TemplateView):
 	template_name = 'flood_hazard.html'
 
 	def post(self, request, *args, **kwargs):
-		return redirect('drought_home')
+		try:
+			import urllib2
+			import simplejson
+			postdata = request.POST.copy()
+			alert_type = postdata['alert_type']
+			hazard_type = postdata['hazard_type']
+			print 'ALERT TYPE: {0}'.format(alert_type)
+			print 'HAZARD TYPE: {0}'.format(hazard_type)
+			req = urllib2.Request('http://maps.virtualkenya.org/geoserver/wfs?srsName=EPSG%3A4326&typename=geonode%3Adrought_constituencies&outputFormat=json&version=1.0.0&service=WFS&request=GetFeature')
+			opener = urllib2.build_opener()
+			f = opener.open(req)
+			data = simplejson.load(f)
+			if (alert_type == 'SMS'):
+				gateway = AfricasTalkingGateway('upande', '5da433821fa3efe8d793bfb95da80e63a4f2909c559ea58cc5a8e096fa568965')
+				for item in data['features']:
+					msisdn = item['properties']['SNIPPET']
+					print msisdn
+					gateway.sendMessage(msisdn, '{0} alert SMS from Early Warning System'.format(hazard_type))
+				gateway.sendMessage('+254722659526', '{0} alert SMS from Early Warning System'.format(hazard_type))
+			elif (alert_type == 'EMAIL'):
+				mandrill_client = mandrill.Mandrill('17dld1KJhCspkCV6zlfbtA')
+				message = {
+					'from_email': 'ews@upande.com',
+					'subject':'{0} early warning'.format(hazard_type),
+					'text':'This is an early warning email'
+				}
+				to_list = []
+				for item in data['features']:
+					email_address = item['properties']['SNIPPET2']
+					constituency = item['properties']['CONSTITUEN']
+					to_list.append(
+						{'email': email_address, 'name': constituency, 'type':'to'}
+					)
+				message.update({'to': to_list})
+				result = mandrill_client.messages.send(message=message, async=False, ip_pool='Main Pool')
+		except Exception as ex:
+			print 'exception: {0}'.format(ex.message)
+			pass
+			
+		return redirect(reverse('flood_home'))
