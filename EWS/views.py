ecb3810cab59a8cc460bc48bb5f0b0359fbf2b5c
Peter Matogo <pmatogo@gmail.com>
2015-03-10 04:29:56 +0000

    Implementing changes required for demo 1. Changing tab heading for date specific tab 2. changing the text on SMS/Email alerts

--- EWS/views.py
+++ EWS/views.py
@@ -28,25 +28,26 @@ class DroughtView(TemplateView):
 				gateway = AfricasTalkingGateway('upande', '5da433821fa3efe8d793bfb95da80e63a4f2909c559ea58cc5a8e096fa568965')
 				for item in data['features']:
 					msisdn = item['properties']['SNIPPET']
+					locale = item['properties']['CONSTITUEN']
 					print msisdn
-					gateway.sendMessage(msisdn, '{0} alert SMS from Early Warning System'.format(hazard_type))
+					gateway.sendMessage(msisdn, 'Alert: Drought risk for {0} (dek 3 March 2011) is worsening; a dry spell is expected with an early Alarm scenario developing in the course of wet season (March-May 2011).'.format(locale))
 				gateway.sendMessage('+254722659526', '{0} alert SMS from Early Warning System'.format(hazard_type))
 			elif (alert_type == 'EMAIL'):
 				mandrill_client = mandrill.Mandrill('17dld1KJhCspkCV6zlfbtA')
-				message = {
-					'from_email': 'ews@upande.com',
-					'subject':'{0} early warning'.format(hazard_type),
-					'text':'This is an early warning email'
-				}
-				to_list = []
 				for item in data['features']:
+					to_list = []					
 					email_address = item['properties']['SNIPPET2']
-					constituency = item['properties']['CONSTITUEN']
+					locale = item['properties']['CONSTITUEN']
+					message = {
+						'from_email': 'ews@upande.com',
+						'subject':'{0} early warning'.format(hazard_type),
+						'text':'Alert: Drought risk for {0} (dek 3 March 2011) is worsening; a dry spell is expected with an early Alarm scenario developing in the course of wet season (March-May 2011).'.format(locale)
+					}					
 					to_list.append(
-						{'email': email_address, 'name': constituency, 'type':'to'}
+						{'email': email_address, 'name': locale, 'type':'to'}
 					)
-				message.update({'to': to_list})
-				result = mandrill_client.messages.send(message=message, async=False, ip_pool='Main Pool')
+					message.update({'to': to_list})
+					result = mandrill_client.messages.send(message=message, async=False, ip_pool='Main Pool')
 		except Exception as ex:
 			print 'exception: {0}'.format(ex.message)
 			pass
@@ -74,14 +75,14 @@ class FloodView(TemplateView):
 				for item in data['features']:
 					msisdn = item['properties']['SNIPPET']
 					print msisdn
-					gateway.sendMessage(msisdn, '{0} alert SMS from Early Warning System'.format(hazard_type))
-				gateway.sendMessage('+254722659526', '{0} alert SMS from Early Warning System'.format(hazard_type))
+					gateway.sendMessage(msisdn, 'Alert: Flood depths of up to 3m are predicted for large parts of Nzoia in the next 3 days, please contact all cooperating agencies.')
+				gateway.sendMessage('+254722659526', 'Alert: Flood depths of up to 3m are predicted for large parts of Nzoia in the next 3 days, please contact all cooperating agencies.')
 			elif (alert_type == 'EMAIL'):
 				mandrill_client = mandrill.Mandrill('17dld1KJhCspkCV6zlfbtA')
 				message = {
 					'from_email': 'ews@upande.com',
 					'subject':'{0} early warning'.format(hazard_type),
-					'text':'This is an early warning email'
+					'text':'Alert: Flood depths of up to 3m are predicted for large parts of Nzoia in the next 3 days, please contact all cooperating agencies.'
 				}
 				to_list = []
 				for item in data['features']:
