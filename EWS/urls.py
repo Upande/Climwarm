f75472952fda6802335996919d3a73f51b695aae
peter.matogo <pmatogo@gmail.com>
2015-04-30 21:24:07 +0300

    adding fall-back url

--- EWS/urls.py
+++ EWS/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 from .views import *
 
@@ -18,4 +19,5 @@ urlpatterns = patterns('',
             {'template_name': 'login.html'}, name='auth_login'),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout_then_login', name='auth_logout'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^.*$', RedirectView.as_view(url='/', permanent=False), name='default'),
 )
